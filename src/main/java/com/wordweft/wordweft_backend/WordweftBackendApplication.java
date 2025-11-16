bac557ea8e29ead593472a187827e5d43b88f2b0
Diksha Mishra <dikshamishra500dm@gmail.com>
2025-11-16 16:49:54 +0530

    Wordweft Backend Application

---
+++ src/main/java/com/wordweft/wordweft_backend/WordweftBackendApplication.java
@@ -0,0 +1,13 @@
+package com.wordweft.wordweft_backend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WordweftBackendApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(WordweftBackendApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/wordweft/wordweft_backend/WordweftBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.wordweft.wordweft_backend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class WordweftBackendApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
